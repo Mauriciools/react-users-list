b4a09e38fae1fa82ac2a82959e6ab84f4dd3b524
Mauricio Salvador <msalvador03@gmail.com>
2023-09-07 22:40:09 -0300

    Implemented portals to error window

--- src/components/General/ErrorWindow.js
+++ src/components/General/ErrorWindow.js
@@ -1,24 +1,35 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "./Card";
 import Button from "./Button";
 import "./ErrorWindow.css";
 
+function Backdrop(props) {
+    return <div className="backdrop" onClick={props.onConfirm}></div>;
+};
+
+function Overlay(props) {
+    return (
+        <Card className="error-window">
+            <header>
+                <h2>{props.title}</h2>
+            </header>
+            <div className="error-content">
+                <p>{props.message}</p>
+            </div>
+            <footer>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
 function ErrorWindow(props) {
     return (
         <div>
-            <div className="backdrop" onClick={props.onConfirm}></div>
-            <Card className="error-window">
-                <header>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className="error-content">
-                    <p>{props.message}</p>
-                </div>
-                <footer>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Overlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))}
         </div>
     );
 };
